6480b2cb155a789eae4cbe42245c93a7b74ecf21
zhaohuayu <zhaohuayu@didichuxing.com>
2017-04-12 15:11:22 +0800

    FactoryBean study

---
+++ src/main/java/com/huayu/study/learnSpring/common/bo/factoryBeanTest/Application.java
@@ -0,0 +1,17 @@
+package com.huayu.study.learnSpring.common.bo.factoryBeanTest;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Created by zhaohuayu on 17/4/10.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("factoryBean.xml"));
+        System.out.println(JSON.toJSONString(beanFactory.getBean("car")));
+    }
+}
---
+++ src/main/java/com/huayu/study/learnSpring/common/bo/factoryBeanTest/CarFactoryBean.java
@@ -0,0 +1,37 @@
+package com.huayu.study.learnSpring.common.bo.factoryBeanTest;
+
+import org.omg.CORBA.DoubleHolder;
+import org.springframework.beans.factory.FactoryBean;
+
+/**
+ * Created by zhaohuayu on 17/4/12.
+ */
+public class CarFactoryBean implements FactoryBean<Car> {
+
+    private String carInfo;
+
+    public Car getObject() throws Exception {
+        Car car = new Car();
+        String[] infos = carInfo.split(",");
+        car.setBrand(infos[0]);
+        car.setMaxSpeed(Integer.parseInt(infos[1]));
+        car.setPrice(Double.valueOf(infos[2]));
+        return car;
+    }
+
+    public Class<?> getObjectType() {
+        return Car.class;
+    }
+
+    public boolean isSingleton() {
+        return false;
+    }
+
+    public String getCarInfo() {
+        return carInfo;
+    }
+
+    public void setCarInfo(String carInfo) {
+        this.carInfo = carInfo;
+    }
+}
